4c51cdb7e9f9506015012ae32491f3e4d1510b03
tsound922 <oushirx@gmail.com>
2017-08-14 18:42:46 +1000

    Please enter the commit message for your changes. Lines starting

--- public/app/controllers/recommCtrl.js
+++ public/app/controllers/recommCtrl.js
@@ -1,10 +1,11 @@
 angular.module('commendController',[])
-.controller('recommCtrl',function ($http)){
+	.controller('recommCtrl',function ($http){
 	this.recommAdd = function(recommData, userName){
 		console.log('Test: recommend and name are submitted');
 		console.log(this.recommData, this.userName);
 		//check about the recommendation message and name
-		$http.post('',this.recommData, this.userName)
+
 	}
-}
+	}
+
 //This js is try to put the recommendation into mongoDB, work as a controller
