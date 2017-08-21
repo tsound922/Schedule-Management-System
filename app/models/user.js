cc80bf9e636e0badf3bae3697359cb552a05bb8d
Farhan Saeed <Farhan Saeed>
2017-08-21 16:43:37 +1000

    Files cleaned up user.js and userServices.js

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
+
+//Form data validation
 var UserSchema = new Schema({
 	username: {type: String, lowercase:true, required:true, unique:true},
 	password: {type: String, required:true},
@@ -8,33 +10,14 @@ var UserSchema = new Schema({
 
 });
 
+//Used to encrypt password
 UserSchema.pre('save', function(next) {
-	var user = this;
-	//password cryption;
-	bcrypt.hash(user.password,null,null,function (err,hash) {
+	bcrypt.hash(this.password,null,null,function (err,hash) {
 		if(err) return next();
-		user.password = hash;
+		this.password = hash;
 		next();
 	});
 });
 
-bcrypt.hash("bacon", null, null, function(err, hash) {
-	// Store hash in your password DB.
-});
-
+//Exporting data to the model User
 module.exports = mongoose.model('User',UserSchema);
-
-/*module.exports = mongoose.model('User', userSchema);
-
-var blogSchema = new Schema({
-	title:  String,
-	author: String,
-	body:   String,
-	comments: [{ body: String, date: Date }],
-	date: { type: Date, default: Date.now },
-	hidden: Boolean,
-	meta: {
-		votes: Number,
-		favs:  Number
-	}
-});*/
