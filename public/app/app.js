c3ac00c9c7d7df6086a676dee47c0d377fd5ad4d
tsound922 <oushirx@gmail.com>
2017-08-24 16:04:15 +1000

    Open source "animate.css" is added

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,3 @@
 //the app(modules) you use. Like a app functions/modules index
-angular.module('userApp',['appRoutes','userControllers','userServices']);
+angular.module('userApp',['appRoutes','userControllers','userServices','ngAnimate']);
 
