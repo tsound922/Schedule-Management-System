ebbacc8ba105fdc849487921a29abc41fa008a19
tsound922 <oushirx@gmail.com>
2017-08-30 01:02:48 +1000

    Login user will get the authentication token, need testing

--- public/app/services/authServices.js
+++ public/app/services/authServices.js
@@ -1,15 +1,43 @@
 angular.module('authServices', [])
-	.factory('Auth',function ($http) {
+	.factory('Auth',function ($http,AuthToken) {
 		var authFactory  = {};
 
 		//send data to back end
 		authFactory.login = function (loginData) {
-			return $http.post('/api/authenticate', loginData);
+			return $http.post('/api/authenticate', loginData).then(function (data) {
+				//console.log(data.data.token);
+				AuthToken.setAuthToken(data.data.token);
+				return data;
+			});
+		};
+
+		/*----------------
+		* Test the user who login will get a auth token or not
+		------------------*/
+		authFactory.loggedIn = function () {
+			if(AuthToken.getAuthToken()){
+				return true;
+			}else{
+				return false;
+			}
 		}
+
 		return authFactory;
 	})
 
-.factory('AuthToken', function () {
+.factory('AuthToken', function ($window) {
 	var authTokenFactory = {};
+
+
+	//AuthToken.setAuthToken(token)
+	//set the token for authentication
+	authTokenFactory.setAuthToken = function (authtoken) {
+		$window.localStorage.setItem('token', authtoken);
+	};
+
+	authTokenFactory.getAuthToken = function () {
+		return $window.localStorage.getItem('token');
+	}
+
 	return authTokenFactory;
 });
