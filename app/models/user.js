4c86ae0ec4ac2d4d122dceba014bb39947806e23
tsound922 <oushirx@gmail.com>
2017-10-04 02:28:09 +1100

    Bug of unable to encrypt the password is fixed.

--- app/models/user.js
+++ app/models/user.js
@@ -10,17 +10,17 @@ var UserSchema = new Schema({
 
 });
 
-//Used to encrypt password
-UserSchema.pre('save', function(next) {
-	bcrypt.hash(this.password,null,null,function (err,hash) {
-		if(err) return next();
-		this.password = hash;
-		next();
-	});
-});
+UserSchema.pre('save',function(next){
+	var user = this;
+	bcrypt.hash(user.password,null,null,function (err,hash) {
+		if(err) return next(err);
+		user.password = hash;
+        next();
+    });
 
+});
 UserSchema.methods.comparePassword = function (password) {
-	return bcrypt.compareSync(password, this.password);
+    return bcrypt.compareSync(password, this.password);
 };
 //Exporting data to the model User
 module.exports = mongoose.model('User',UserSchema);
