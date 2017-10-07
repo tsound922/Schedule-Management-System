cde38eae021d5cf9afc529a6fc5ea6d89bb28e16
tsound922 <oushirx@gmail.com>
2017-10-07 23:37:44 +1100

    Separate the database connection with server file for security.

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var appRoutes = require('./app/routes/api')(router);
 var path = require('path');
+var dbconfig = require('./db_config')
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use('/api', appRoutes);
-
+dbconfig();
 app.get('*', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
