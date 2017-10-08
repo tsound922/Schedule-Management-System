07c0cc64c9c6ecf8cfb3d49253b3551e585d53c4
Aeroidian <neo_mai_kuraki@hotmail.com>
2017-10-08 17:14:29 +0800

    in-app feedback added.

--- public/app/controllers/emailCtrl.js
+++ public/app/controllers/emailCtrl.js
@@ -4,9 +4,25 @@ angular.module('emailController', ['userServices'])
     
         app = this;
     
-        app.sendUsername = function(userData){
-            User.sendUsername(app.userData.email).then(function(data){
-                console.log(data);
-            });
+        app.sendUsername = function(userData, valid){
+            app.erroMsg = false;
+            app.loading = true;
+            app.disabled = true;
+            
+            if (valid) {
+                User.sendUsername(app.userData.email).then(function(data){  
+                    app.loading = false;
+                    if (data.data.success) {
+                        app.successMsg = data.data.message; 
+                    } else {
+                        app.disabled = false; 
+                        app.errorMsg = data.data.message; 
+                    }
+                });
+            } else {
+                app.disabled = false; 
+                app.loading = false; 
+                app.errorMsg = 'Please enter a valid e-mail'; 
+            }
         };
-    });
+    })
